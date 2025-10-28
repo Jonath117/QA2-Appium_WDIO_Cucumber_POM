09e9c00f61f1e0d4038cae043b193c088c0d1e74
Jonathan <rochacontrerasjonathan40@gmail.com>
2025-10-28 03:55:13 -0400

    arreglo wdio.conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -63,7 +63,7 @@ exports.config = {
     
 // wdio.conf.js (ejemplo)
     // ...
-    hostname: 'http://hub-cloud.browserstack.com/wd/hub',
+    hostname: 'hub-cloud.browserstack.com',
     port: 443, // o 80 para conexiones no seguras
     // ...
     
